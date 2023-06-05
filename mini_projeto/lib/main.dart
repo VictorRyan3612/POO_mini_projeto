4ad31c052a1155b71949ba4dbbb571a712d6818a
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-05 20:36:32 -0300

    Teste requisição API v.1.1.0

--- mini_projeto/lib/main.dart
+++ mini_projeto/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MainApp());
@@ -36,7 +37,9 @@ class HomePageApp extends HookWidget{
   Widget build(BuildContext context) {
     return
     Scaffold(
-      appBar: MyAppBar(_currentBrightness)
+      appBar: MyAppBar(_currentBrightness),
+
+      body: GamesList(),
     );
   }
 }
@@ -82,3 +85,41 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
 }
 
 
+class GamesList extends HookWidget {
+  const GamesList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var fim = useState("Não testado");
+
+    Future<void> fetchData() async {
+      var gamesUri = Uri(
+        scheme: 'https',
+        host: 'freetogame.com',
+        path: 'api/games',
+      );
+
+      try {
+        var jsonString = await http.read(gamesUri);
+        var uriJson = jsonDecode(jsonString);
+        fim.value = "Sucesso";
+      } 
+
+      catch (error) {
+        var a = error.runtimeType.toString();
+        fim.value = "Erro $a";
+      }
+    }
+
+    fetchData();
+    
+    return Center(
+      child:
+        Text("${fim.value}", style: TextStyle(fontSize: 20),)
+
+    );
+
+
+
+  }
+}
