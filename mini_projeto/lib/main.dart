3957f1ecfebe247c92a9b25dbb3f203f4ffa1e28
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-06 00:04:21 -0300

    Melhorias v3.0.1

--- mini_projeto/lib/main.dart
+++ mini_projeto/lib/main.dart
@@ -123,19 +123,26 @@ class GamesList extends HookWidget {
     }
 
     if (fim.value == "Sucesso"){
-      return ListView.builder(
-        itemCount: games.value.length,
-        itemBuilder: (BuildContext context, int index) {
-          final game = games.value[index];
-          return InkWell(
-            onTap: () => navigateToGameDetails(context, game),
-            child: ListTile(
-              title: Text(game['title']),
-              subtitle: Text(game['genre']),
-              leading: Image.network(game['thumbnail']),
-            ),
-          );
-        }
+      
+      return Center(
+        child: ListView.builder(
+          itemCount: games.value.length,
+          itemBuilder: (BuildContext context, int index) {
+            final game = games.value[index];
+
+            return InkWell(
+              onLongPress: () => print(game),
+              onTap: () => navigateToGameDetails(context, game),
+              child: ListTile(
+                title: Text(game['title']),
+                subtitle: Text(game['genre']),
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.network(game['thumbnail'])),
+              ),
+            );
+          }
+        ),
       );
     }
 
@@ -154,6 +161,7 @@ class GameDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dynamic game = ModalRoute.of(context)?.settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(game["title"]),
@@ -163,13 +171,17 @@ class GameDetailsScreen extends StatelessWidget {
         child: Row(
           mainAxisAlignment:MainAxisAlignment.center ,
           children: [
-            Image.network(
-              game['thumbnail'],
-              height: 200,
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                game['thumbnail'],
+                height: 200,
+              ),
             ),
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: gamesinfo.map((item) {
+
                 return Padding(
                   padding: const EdgeInsets.all(5),
                   child: 
@@ -219,13 +231,13 @@ class GameDetailsScreen extends StatelessWidget {
 final List<Map<String, dynamic>> gamesinfo = [
   
   {
-    'nameProp': 'Title:',
+    'nameProp': 'Título:',
     'prop': 'title',
     'fontsize': 18.0,
     'height': 16.0
   },
   {
-    'nameProp': 'Genero:',
+    'nameProp': 'Gênero:',
     'prop': 'genre',
     'fontsize': 18.0,
     'height': 16.0
