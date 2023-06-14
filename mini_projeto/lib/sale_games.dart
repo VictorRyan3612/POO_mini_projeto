04b1744a5e065a85b8e1d04e5898ce512bf10c42
Victor <117222760+VictorRyan36122@users.noreply.github.com>
2023-06-14 02:18:06 -0300

    Mirror Bug v3.9.2

--- mini_projeto/lib/sale_games.dart
+++ mini_projeto/lib/sale_games.dart
@@ -149,41 +149,48 @@ class SaleGamesDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(game["title"]),
       ),
+
       body: SafeArea(
-        
         child: Center(
-          
-          child: Column(
-            mainAxisAlignment:MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: Image.network(
-                    game['thumb'],
-                    height: 200,
+          child: SingleChildScrollView(
+
+            child: Column(
+              mainAxisAlignment:MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      game['thumb'],
+                      height: 200,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: steamGamesInfo.map((item) {
-              
-                    return Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: 
-                        Text('${item['nameProp']} \t${game[item['prop']]}',
-                          style: TextStyle(fontSize: item['fontsize'])
-                        )
-                    );
-                  }).toList()
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: steamGamesInfo.map((item) {
+                
+                      return Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: 
+                            Text('${item['nameProp']} \t${game[item['prop']]}',
+                              style: TextStyle(fontSize: item['fontsize'])
+                            )
+                        ),
+                      );
+                    }).toList()
+                  ),
                 ),
-              ),
-            ],
+                
+              ],
+            ),
           ),
         ),
       ),
