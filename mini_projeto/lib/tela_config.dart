ce021d19bf0e5e4c883d7a3222f0a794ba37db47
Victor Ryan <vitorsilva3612@gmail.com>
2024-04-18 14:19:46 -0300

    fix: remocao de dependencia cardSettings

--- mini_projeto/lib/tela_config.dart
+++ mini_projeto/lib/tela_config.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:card_settings/card_settings.dart';
 
 class TelaConfigs extends HookWidget implements PreferredSizeWidget{
   final ValueNotifier<Brightness> currentBrightness;
@@ -27,31 +26,7 @@ class TelaConfigs extends HookWidget implements PreferredSizeWidget{
               constraints: const BoxConstraints(maxWidth: 600),
               child: Padding(
                 padding: const EdgeInsets.all(12),
-                child:CardSettings.sectioned(
-                  children: [
-                    CardSettingsSection(
-                      header: CardSettingsHeader(
-                        label: 'Geral',
-                      ),
-                      children: [
-                        CardSettingsSwitch(
-                          trueLabel: '', 
-                          falseLabel: '',
-                          label: 'Modo noturno',
-                          initialValue: true,
-                          onChanged: (value) {
-                            if (currentBrightness.value == Brightness.dark) {
-                              currentBrightness.value = Brightness.light;
-                            } 
-                            else {
-                              currentBrightness.value = Brightness.dark;
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                child: Container()
               )
             ),
           ),
