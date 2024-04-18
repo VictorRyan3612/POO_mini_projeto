fbede7e74c1a774f709262f8287e4b284633188c
Victor Ryan <vitorsilva3612@gmail.com>
2024-04-18 14:33:33 -0300

    refact: config screen

--- mini_projeto/lib/tela_config.dart
+++ mini_projeto/lib/tela_config.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class TelaConfigs extends HookWidget implements PreferredSizeWidget{
+class TelaConfigs extends HookWidget{
   final ValueNotifier<Brightness> currentBrightness;
 
   const TelaConfigs({required this.currentBrightness, super.key});
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tela de configurações'),
-      ),
-
+      appBar: AppBar(title: Text("Configurações"),),
 
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: 600),
-              child: Padding(
-                padding: const EdgeInsets.all(12),
-                child: Container()
-              )
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: ListView(
+                children: [
+                  ListTile(),
+                ],
+              ),
             ),
           ),
         ),
